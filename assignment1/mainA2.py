eb09f5482723f5c0fc6f75a0e73c4ec69633247b
Eldad Haber <eldadHaber@gmail.com>
2020-01-23 11:10:30 -0800

    added assignmet1

---
+++ assignment1/mainA2.py
@@ -0,0 +1,61 @@
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+plt.ion()
+
+def misfit(a,b):
+    n = a.numel()
+    return torch.norm(a-b)/n       
+
+
+def getGradients(T,x,y):
+
+    dummy = torch.zeros(3,4)
+    Tx = dummy
+    Ty = dummy
+    # your code here to compute Tx, Ty
+    
+    return Tx, Ty
+
+def getAbsGrad(Tx,Ty):
+    
+    dummy = torch.zeros(3,4)
+    absGradT = dummy
+    # your code here to compute absGradT = sqrt(Tx^2 + Ty^2)
+    
+    return absGradT
+
+
+if __name__ == "__main__":
+     
+    # load topo file
+    x = torch.load('x.pt')
+    # load y and topo here
+    #
+    #
+    
+    #plt.imshow(topo, extent=[x.min(), x.max(), y.min(), y.max()])
+    #plt.show()
+    
+    # Compute the gradient
+    Tx, Ty =  getGradients(topo,x,y)
+    
+    
+    # Compute |\grad T|
+    absGradT = getAbsGrad(Tx,Ty)
+
+
+    # load correct answer
+    #absGradTtrue = 
+    
+    # compare
+    epsilon = 0.001
+    pss = 0
+    if misfit(absGradT,absGradTtrue)<epsilon:
+        pss = 1
+        
+    if pss == 1:
+        print('Test Pass')
+    else:
+        print('Test Fail')
+ 
